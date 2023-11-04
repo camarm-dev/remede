c83c52cec1bbb69e6f9cbb5467a5b862408a70ee
camarm <contact@camarm.dev>
2023-11-04 17:33:48 +0100

    [Add] API grâce à Fastapi
    Signed-off-by: camarm <armand@camponovo.xyz>

---
+++ server.py
@@ -0,0 +1,58 @@
+import json
+
+import uvicorn
+from fastapi import FastAPI
+
+version = "1.0.0"
+app = FastAPI(title='Remède', description='Un dictionnaire libre.', version=version)
+transformLetter = {
+    'â': 'a',
+    'æ': 'a',
+    'à': 'a',
+    'ç': 'c',
+    'î': 'i',
+    'ï': 'i',
+    'ù': 'u',
+    'û': 'u',
+    'ü': 'u',
+    'é': 'e',
+    'ë': 'e',
+    'ê': 'e',
+    'è': 'e'
+}
+
+
+def get_remede_json(letter: str):
+    return json.loads(open(f'data/REMEDE_{letter}.json').read())
+
+
+def get_first_letter(word: str):
+    first_letter = word[0]
+    if first_letter not in transformLetter.keys():
+        return first_letter
+    return transformLetter[first_letter]
+
+
+def get_remede_doc(word: str):
+    return REMEDE[get_first_letter(word)].get(word, {'message': 'Mot non trouvé'})
+
+
+@app.get('/')
+def root():
+    return {
+        "version": version,
+        "message": "Check /docs for documentation"
+    }
+
+
+@app.get('/word/{word}')
+def get_word_document(word: str):
+    return get_remede_doc(word)
+
+
+if __name__ == '__main__':
+    REMEDE = {
+        'a': get_remede_json('a'),
+        'b': get_remede_json('b')
+    }
+    uvicorn.run(app, host='0.0.0.0')
