c7c71a797c557cb5ba244b14abd7c2ac92261b5f
camarm <armand@camponovo.xyz>
2023-11-11 00:04:07 +0100

    [Add] Modifications temporaire
    Signed-off-by: camarm <armand@camponovo.xyz>

--- parse.py
+++ parse.py
@@ -135,11 +135,12 @@ def remedize(word_list: list):
         'c': ['ç'],
         'i': ['î', 'ï'],
         'u': ['ù', 'û', 'ü'],
-        "e": ['é', 'ê', 'è', 'ë']
+        "e": ['é', 'ê', 'è', 'ë'],
+        'o': ['ô', 'ö', 'œ']
     }
-    current_char = 'j'
+    current_char = 'm'
     for word in word_list:
-        if word.lower().startswith('a') or any([word.lower().startswith(char) for char in accepted_char['a']]) or any([word.lower().startswith(char) for char in accepted_char['c']]) or any([word.lower().startswith(char) for char in accepted_char['i']]) or word.lower().startswith('b') or word.lower().startswith('c') or word.lower().startswith('d') or word.lower().startswith('e') or any([word.lower().startswith(char) for char in accepted_char['e']]) or word.lower().startswith('f') or word.lower().startswith('g') or word.lower().startswith('h') or word.lower().startswith('i'):
+        if word.lower()[0] in 'abcdefghijkl' or any([word.lower().startswith(char) for char in accepted_char['a']]) or any([word.lower().startswith(char) for char in accepted_char['c']]) or any([word.lower().startswith(char) for char in accepted_char['i']]) or word.lower().startswith('b') or any([word.lower().startswith(char) for char in accepted_char['e']]):
             continue
         if not word.lower().startswith(current_char) and not any([word.lower().startswith(char) for char in accepted_char.get(current_char, [current_char])]):
             saveRemede(current_char, remede_dictionary)
