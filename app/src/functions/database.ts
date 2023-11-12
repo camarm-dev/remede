6ebbd4c66e7675f386c5bdedfe9204d03058c206
camarm <armand@camponovo.xyz>
2023-11-12 19:18:15 +0100

    [Add] Attender que la base soit chargée, messages d'erreurs
    Signed-off-by: camarm <armand@camponovo.xyz>

--- app/src/functions/database.ts
+++ app/src/functions/database.ts
@@ -1,12 +1,24 @@
 import {getRawDictionary} from "@/functions/offline";
 import initSqlJS, {Database} from "sql.js"
+import {toastController} from "@ionic/vue";
 
 async function openDatabase() {
-    const SQL = await initSqlJS({
-        locateFile: file => `/sql-wasm.wasm`
-    })
-    const raw = await getRawDictionary()
-    return new SQL.Database(raw)
+    try {
+        const SQL = await initSqlJS({
+            locateFile: file => `/sql-wasm.wasm`
+        })
+        const raw = await getRawDictionary()
+        return new SQL.Database(raw)
+    } catch (e) {
+        const message = await toastController.create({
+            header: 'Erreur',
+            message: `L'ouverture de la BDD à échouée: ${e}`,
+            duration: 5000,
+            color: 'danger'
+        })
+
+        await message.present()
+    }
 }
 
 class RemedeDatabase {
@@ -25,21 +37,28 @@ class RemedeDatabase {
 
     async getWord(word: string) {
         const statement = `SELECT document FROM dictionary WHERE word = '${word}'`
-        const response = this.query(statement)
+        const response = await this.query(statement)
         return JSON.parse(response[0])
     }
 
     async getAutocomplete(query: string) {
         const statement = `SELECT word FROM dictionary WHERE word LIKE '${query}%' LIMIT 6`
-        return this.query(statement)
+        return await this.query(statement)
     }
 
     async getRandomWord() {
         const statement = "SELECT word FROM dictionary ORDER BY RANDOM() LIMIT 1"
-        return this.query(statement)[0]
+        return await this.query(statement)[0]
     }
 
-    query(statement: string) {
+    async query(statement: string): Promise<Array<string>> {
+        if (!this.db) {
+            return await new Promise((resolve) => {
+                setTimeout(() => {
+                    resolve(this.query(statement))
+                }, 500)
+            })
+        }
         const results = this.db.exec(statement)
         const parsed = []
 
