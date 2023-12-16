bec58da62b3ab4dc1c8022f9e9966652f1cf34ad
camarm <armand@camponovo.xyz>
2023-12-16 14:32:36 +0100

    [Add] Écriture d'un premier test cypress
    Signed-off-by: camarm <armand@camponovo.xyz>

--- app/tests/e2e/specs/test.cy.ts
+++ app/tests/e2e/specs/test.cy.ts
@@ -1,6 +1,25 @@
-describe('My First Test', () => {
+describe('Page routing checker', () => {
   it('Visits the app root url', () => {
-    cy.visit('/folder/Inbox')
-    cy.contains('#container', 'Inbox')
+    cy.visit('/dictionnaire')
+  })
+
+  it('Visits the bookmark page url', () => {
+    cy.visit('/marques-page')
+  })
+
+  it('Visits the correction page url', () => {
+    cy.visit('/correction')
+  })
+
+  it('Visits the sheets page url', () => {
+    cy.visit('/fiches')
+  })
+
+  it('Visits the settings page url', () => {
+    cy.visit('/parametres')
+  })
+
+  it('Visits the about page url', () => {
+    cy.visit('/a-propos')
   })
 })
