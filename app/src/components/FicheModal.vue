6fcf912aa68a2f99dc0a7120d061806a0bd58c15
camarm <armand@camponovo.xyz>
2023-12-17 13:34:08 +0100

    [Fix] #50 : Partager une ficher
    Signed-off-by: camarm <armand@camponovo.xyz>

--- app/src/components/FicheModal.vue
+++ app/src/components/FicheModal.vue
@@ -99,7 +99,7 @@ export default defineComponent({
         await Share.share({
           title: `"${this.nom}" sur Remède`,
           text: `La fiche de français "${this.nom}" est sur Remède !`,
-          url: `https://remede.camam.fr/fiches/${this.slug}`,
+          url: `https://remede-app.camarm.fr/fiches/${this.slug}`,
           dialogTitle: 'Partager la fiche',
         })
       } catch {
