a665e3c7d2a14fba13e55e44a47beff642ad05a6
camarm <armand@camponovo.xyz>
2024-01-05 13:49:28 +0100

    [Add] Added electron app #53
    Signed-off-by: camarm <armand@camponovo.xyz>

---
+++ app/electron/capacitor.config.ts
@@ -0,0 +1,31 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.camarm.remede',
+  appName: 'Remède',
+  webDir: 'dist',
+  plugins: {
+    "SplashScreen": {
+      "launchAutoHide": false,
+      "androidScaleType": "CENTER_CROP",
+      "splashFullScreen": true,
+      "splashImmersive": false,
+      "backgroundColor": "#ffffff"
+    },
+    CapacitorHttp: {
+      enabled: false
+    }
+  },
+  server: {
+    androidScheme: 'https'
+  },
+  electron: {
+    deepLinkingEnabled: true,
+    deepLinkingCustomProtocol: 'remede',
+    splashScreenImageName: "splash.png",
+    splashScreenEnabled: true,
+    customUrlScheme: 'http'
+  }
+};
+
+export default config;
