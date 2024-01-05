f6b27bd3d0a6a2854b47339dc6325f9c9b922e7b
camarm <armand@camponovo.xyz>
2024-01-05 19:38:25 +0100

    [Add] Builds configuration #53
    Signed-off-by: camarm <armand@camponovo.xyz>

--- app/electron/forge.config.js
+++ app/electron/forge.config.js
@@ -5,8 +5,22 @@ module.exports = {
   rebuildConfig: {},
   makers: [
     {
-      name: '@electron-forge/maker-squirrel',
-      config: {},
+      name: '@electron-forge/maker-wix',
+      config: {
+        name: "Remède",
+        manufacturer: "Labse Software",
+        description: "Open source and free French dictionary !",
+        icon: "./assets/appIcon.ico"
+      },
+    },
+    {
+      name: '@electron-forge/maker-dmg',
+      config: {
+        name: "Remède",
+        background: './assets/splash.png',
+        format: 'ULFO',
+        icon: "./assets/appIcon.ico"
+      }
     },
     {
       name: '@electron-forge/maker-zip',
@@ -14,11 +28,36 @@ module.exports = {
     },
     {
       name: '@electron-forge/maker-deb',
-      config: {},
+      config: {
+        options: {
+          name: "Remède",
+          description: "Open source and free French dictionary !",
+          productName: "Remède",
+          productDescription: "Remède is an open source alternative to Antidote. It's free and open source !",
+          categories: ["Utility"],
+          genericName: "Dictionary",
+          homepage: "https://remede.camarm.fr",
+          icon: "./assets/appIcon.ico",
+          maintainer: "Armand CAMPONOVO (@camarm-dev)"
+        }
+      },
     },
     {
       name: '@electron-forge/maker-rpm',
-      config: {},
+      config: {
+        options: {
+          name: "Remède",
+          description: "Open source and free French dictionary !",
+          productName: "Remède",
+          productDescription: "Remède is an open source alternative to Antidote. It's free and open source !",
+          categories: ["Utility"],
+          genericName: "Dictionary",
+          homepage: "https://remede.camarm.fr",
+          icon: "./assets/appIcon.ico",
+          maintainer: "Armand CAMPONOVO (@camarm-dev)",
+          license: "CeCill V2.1"
+        }
+      },
     },
   ],
   plugins: [
