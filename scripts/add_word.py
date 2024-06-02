abf12107523674c74d019e1542c37c88db468160
camarm-dev <contact@camarm.dev>
2024-06-02 14:51:26 +0200

    add: fix #152; script to quickly add a new word

---
+++ scripts/add_word.py
@@ -0,0 +1,128 @@
+import datetime
+import json
+import runpy
+import sqlite3
+import sys
+
+from generate_sqlite import get_remede_json
+from generate_index import sanitize_word
+from parse import get_word_document, reverse_accepted_char, saveRemede
+
+
+def insert_document(word_document: dict, word: str):
+    cursor.execute("INSERT INTO dictionary VALUES (?,?)", (word, json.dumps(word_document)))
+
+
+def add_to_wordlist(word: str, phoneme: str):
+    with open('data/IPA.txt', 'r') as file:
+        content = file.read()
+    with open('data/IPA.txt', 'w') as file:
+        insert = f"{word}\t{phoneme}"
+        if insert in content:
+            print("Word exists. Exiting.")
+            exit()
+        words = content.split('\n')
+        words.append(insert)
+        words.sort(key=lambda val: sanitize_word(val))
+        new_content = "\n".join(words)
+        new_content = new_content.replace("\n", "", 1)
+        file.write(new_content)
+
+
+def add_to_json(word: str, document: dict):
+    letter = reverse_accepted_char.get(word[0], word[0])
+    REMEDE[letter][word] = document
+    saveRemede(letter, REMEDE[letter])
+
+
+def post_setup():
+    cursor.close()
+    database.commit()
+    database.close()
+
+
+def getTimeDetails(time_object):
+    days, seconds = time_object.days, time_object.seconds
+    hours = days * 24 + seconds // 3600
+    minutes = (seconds % 3600) // 60
+    seconds = seconds % 60
+    return hours, minutes, seconds
+
+
+if __name__ == '__main__':
+    REMEDE = {
+        'a': get_remede_json('a'),
+        'b': get_remede_json('b'),
+        'c': get_remede_json('c'),
+        'd': get_remede_json('d'),
+        'e': get_remede_json('e'),
+        'f': get_remede_json('f'),
+        'g': get_remede_json('g'),
+        'h': get_remede_json('h'),
+        'i': get_remede_json('i'),
+        'j': get_remede_json('j'),
+        'k': get_remede_json('k'),
+        'l': get_remede_json('l'),
+        'm': get_remede_json('m'),
+        'n': get_remede_json('n'),
+        'o': get_remede_json('o'),
+        'p': get_remede_json('p'),
+        'q': get_remede_json('q'),
+        'r': get_remede_json('r'),
+        's': get_remede_json('s'),
+        't': get_remede_json('t'),
+        'u': get_remede_json('u'),
+        'v': get_remede_json('v'),
+        'w': get_remede_json('w'),
+        'x': get_remede_json('x'),
+        'y': get_remede_json('y'),
+        'z': get_remede_json('z')
+    }
+
+    before = datetime.datetime.now()
+
+    database = sqlite3.connect('data/remede.db')
+    cursor = database.cursor()
+
+    try:
+        _, word, phoneme = sys.argv
+
+        if not phoneme.startswith("/"):
+            print("Phoneme must be formated like \"/ʁəmɛd/\"")
+            exit()
+
+        print(f"Ajout du mot \"{word}\"...")
+
+        print("- Ajout dans la liste de mots...")
+        add_to_wordlist(word, phoneme)
+        print("Fait.")
+
+        print("- Construction du document Remède...")
+        document = get_word_document(word, phoneme)
+        print("Fait.")
+
+        print("- Ajout du document aux bases JSON")
+        add_to_json(word, document)
+        print("Fait.")
+
+        print("- Ajout du document à la base Sqlite")
+        insert_document(document, word)
+        print("Fait.")
+
+        post_setup()
+
+        print("- Génération des ressources...")
+        runpy.run_module('pre_generate_ressources', run_name='__main__')
+        print("Fait.")
+
+        print("- Génération de l'index...")
+        runpy.run_module('generate_index', run_name='__main__')
+        print("Fait.")
+
+    except Exception as e:
+        print(f"Échec. Assurez vous d'avoir bien fourni les arguments nécessaires. \"{e}\"")
+
+    after = datetime.datetime.now()
+    time = after - before
+    hour, minute, second = getTimeDetails(time)
+    print(f'Fini en {hour} heures {minute} minutes et {second} secondes.')
