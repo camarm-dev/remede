3a04c3f6b4b342ea7b5e48e1d81b0c0ba8d4e090
camarm-dev <contact@camarm.dev>
2024-06-16 13:13:29 +0200

    fix(plugin): added setResult plugin

--- app/android/app/src/main/java/com/camarm/remede/MainActivity.java
+++ app/android/app/src/main/java/com/camarm/remede/MainActivity.java
@@ -1,5 +1,13 @@
 package com.camarm.remede;
 
+import android.os.Bundle;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(SetResult.class);
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/android/app/src/main/java/com/camarm/remede/SetResult.java
@@ -0,0 +1,22 @@
+package com.camarm.remede;
+
+import android.app.Activity;
+import android.content.Intent;
+
+import com.getcapacitor.Plugin;
+import com.getcapacitor.PluginCall;
+import com.getcapacitor.PluginMethod;
+import com.getcapacitor.annotation.CapacitorPlugin;
+
+@CapacitorPlugin(name = "SetResult")
+public class SetResult extends Plugin {
+
+    @PluginMethod()
+    public void sendActiveRemedeResult(PluginCall call) {
+        String value = call.getString("value");
+
+        Intent returnIntent = new Intent().putExtra("com.camarm.remede.ACTIVE_REMEDE_RESULT", value);
+        getActivity().setResult(Activity.RESULT_OK, returnIntent);
+        getActivity().finish();
+    }
+}
