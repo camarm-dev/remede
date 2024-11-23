884cb887ddb51dda3c0d84d8a39a95a7bb08b410
camarm-dev <contact@camarm.dev>
2024-11-23 23:31:13 +0100

    fix: added conjugation source metadata

--- scripts/utils/sources.py
+++ scripts/utils/sources.py
@@ -28,8 +28,13 @@ SOURCES = {
         "identifier": "thesaurus_com",
         "label": "definition.synonyms_and_antonyms",
         "url": "https://www.thesaurus.com/browse/{word}"
+    },
+    "mlconjug3": {
+        "identifier": "mlconjug3",
+        "label": "definition.conjugation",
+        "url": "https://github.com/Ars-Linguistica/mlconjug3"
     }
 }
 
 FRENCH_SOURCES = ["antonyme.org", "synonymo.fr", "conjuguons.fr", "fr.wik"]
-ENGLISH_SOURCES = ["thesaurus.com", "en.wik"]
+ENGLISH_SOURCES = ["thesaurus.com", "en.wik", "mlconjug3"]
