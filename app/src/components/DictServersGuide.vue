5f24a76ee7616d82efd04993d712081149c93107
camarm-dev <contact@camarm.dev>
2025-02-26 15:21:16 +0100

    fix: moved informations about DICT to a component

---
+++ app/src/components/DictServersGuide.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+import {
+  IonContent,
+  IonIcon
+} from "@ionic/vue"
+import {compassOutline, informationCircleOutline, saveOutline, searchOutline} from "ionicons/icons"
+</script>
+
+<template>
+  <ion-content class="ion-padding">
+    <h1>{{ $t('settingsPage.dictServers') }}</h1>
+    <div class="section">
+      <div class="icon">
+        <ion-icon color="primary" :icon="informationCircleOutline"></ion-icon>
+      </div>
+      <p>{{ $t('settingsPage.information1RemedeDictClient') }}</p>
+    </div>
+    <div class="section">
+      <div class="icon">
+        <ion-icon color="primary" :icon="compassOutline"></ion-icon>
+      </div>
+      <p>{{ $t('settingsPage.information2ExploreServers') }}</p>
+    </div>
+    <div class="section">
+      <div class="icon">
+        <ion-icon color="primary" :icon="saveOutline"></ion-icon>
+      </div>
+      <p>{{ $t('settingsPage.information3SaveCustomServers') }}</p>
+    </div>
+    <div class="section">
+      <div class="icon">
+        <ion-icon color="primary" :icon="searchOutline"></ion-icon>
+      </div>
+      <p>{{ $t('settingsPage.information4HowToUse') }}</p>
+    </div>
+  </ion-content>
+</template>
+
+<style scoped>
+.section {
+  display: flex;
+  justify-content: start;
+  align-items: start;
+  gap: 1em;
+  margin-top: 2em;
+}
+
+.section p {
+  margin: 0;
+}
+
+.section .icon ion-icon {
+  background: rgba(var(--ion-color-primary-rgb), .1);
+  margin: 0;
+  padding: .4em;
+  width: 25px;
+  height: 25px;
+  border-radius: 50%;
+}
+</style>
