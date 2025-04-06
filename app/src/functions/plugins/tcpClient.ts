a3bd89b0a913046e89133dd08d76fd3f15653e9f
camarm-dev <contact@camarm.dev>
2025-04-06 14:19:59 +0200

    feat: added fetch using proxy if platform is not android (#280)

--- app/src/functions/plugins/tcpClient.ts
+++ app/src/functions/plugins/tcpClient.ts
@@ -1,9 +1,28 @@
-import { registerPlugin } from "@capacitor/core"
+import {Capacitor, registerPlugin} from "@capacitor/core"
+
+const isAndroid = Capacitor?.getPlatform() == "android"
 
 export interface TCPClientPlugin {
     request(options: { host: string, port: number, messages: string[] }): Promise<{ responses: string[] }>;
 }
 
-const TCPClient = registerPlugin<TCPClientPlugin>("TCPClient")
+// We use native fetch if platform is android. If not, we send DICT requests through the Remède DICT server proxy
+const TCPClient = isAndroid ? registerPlugin<TCPClientPlugin>("TCPClient") : {
+    async request(options: { host: string, port: number, messages: string[] }): Promise<{ responses: string[] }> {
+        return {
+            responses: await fetch("https://api-remede.camarm.fr/proxy/dict", {
+                method: "POST",
+                body: JSON.stringify({
+                    server: options.host,
+                    port: options.port,
+                    commands: options.messages
+                }),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            }).then(response => response.json())
+        }
+    }
+}
 
 export default TCPClient
